d405fc1f13345ed8f1aeae0f5ef2dfe1d3545cad
pliniopvv <gangss2@hotmail.com>
2022-05-13 14:04:08 -0300

    @RestController

---
+++ src/test/java/br/com/pvv/hw/HelloSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pvv.hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
